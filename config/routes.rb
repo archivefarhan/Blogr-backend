e2ec2f50edcb9fe0e8e2782e6e233ca74d89b3b4
farhan <farhanj.mohamed@gmail.com>
2023-02-21 19:58:01 -0800

    added routes and seeded data

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,14 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
+  ### User Routes
   post "/users" => "users#create"
   post "/sessions" => "sessions#create"
+
+  ### Post Routes
+  get "/posts" => "posts#index"
+  get "/posts/:id" => "posts#show"
+  post "/posts/" => "posts#create"
+  patch "/posts/:id" => "posts#update"
+  delete "/posts/:id" => "posts#destroy"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Post.create!(user_id: 1, title: "test1", body: "howdy duty", image: "https://www.testim.io/wp-content/uploads/2019/11/Testim-What-is-a-Test-Environment_-A-Guide-to-Managing-Your-Testing-A.png")
+Post.create!(user_id: 1, title: "test2", body: "hello world!", image: "https://www.testim.io/wp-content/uploads/2019/11/Testim-What-is-a-Test-Environment_-A-Guide-to-Managing-Your-Testing-A.png")
